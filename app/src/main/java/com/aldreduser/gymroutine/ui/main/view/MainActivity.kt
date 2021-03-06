7a68105a2a0bcb911ade1ca80d4cd8d397a09307
Ed Cres <crespoed07@gmail.com>
2021-03-06 02:33:22 -0500

    Added navigate to new workout fab FAB.

--- app/src/main/java/com/aldreduser/gymroutine/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/aldreduser/gymroutine/ui/main/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aldreduser.gymroutine.ui.main.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldreduser.gymroutine.R
@@ -8,15 +9,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 // room codelabs https://developer.android.com/codelabs/android-room-with-a-view-kotlin/#3
 
 // ui
+// todo: add button in the main activity
+// todo: add workout activity
+//toolbar
 // todo: have a toolbar, but it hides when u scroll down
+// todo: toolbar in add workout activity
+// todo: toolbar in add workout activity
+// navigation tabs
+// todo: add navigation tabs in main activity
 
 // database (room)
 // todo: have the workouts organized in different categories (ie chest, arms, legs)
 //  -user can add, delete, edit categories. Can change the names
+//Entity
+// todo: workout goup
+// todo: workout
+// todo: set
+//DAO
+//Database
+//Repo
+//ViewModel
 
 // recyclerview
 // todo: have 2 columns
 // todo: recyclerview displays workouts organized by categories
+
+// Databinding
 
 // future
 // have a history of previous workouts
@@ -28,6 +46,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setUpAppBar()
+
+        fabOnClick()
+    }
+
+    private fun fabOnClick() {
+        // todo: pass some data to that activity
+        // navigate to add workout activity
+        addWorkoutFab.setOnClickListener {
+            val newIntent = Intent(this, AddShoppingItemActivity::class.java)
+            startActivity(newIntent)
+        }
     }
 
     private fun setUpAppBar() {
