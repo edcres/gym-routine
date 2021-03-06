bb418c54d509708b5a5d48cf8a991dc2ffc74ba5
Ed Cres <crespoed07@gmail.com>
2021-03-06 01:22:33 -0500

    Added material dependency, changed colors, and actionbar style.

--- app/src/main/java/com/aldreduser/gymroutine/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/aldreduser/gymroutine/ui/main/view/MainActivity.kt
@@ -4,6 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldreduser.gymroutine.R
 
+// room codelabs https://developer.android.com/codelabs/android-room-with-a-view-kotlin/#3
+
+// ui
+// todo: have a toolbar, but it hides when u scroll down
+
+// database (room)
+// todo: have the workouts organized in different categories (ie chest, arms, legs)
+//  -user can add, delete, edit categories. Can change the names
+
+// recyclerview
+// todo: have 2 columns
+// todo: recyclerview displays workouts organized by categories
+
+// future
+// have a history of previous workouts
+// section for maxes and history of maxes
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
