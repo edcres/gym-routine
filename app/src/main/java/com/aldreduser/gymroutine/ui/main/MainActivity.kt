0592f7bf952efcc09f8d57aad56bc6535028002c
Ed Cres <crespoed07@gmail.com>
2022-03-18 22:05:17 -0400

    Disabled dark mode, plus changed some UI.

--- app/src/main/java/com/aldreduser/gymroutine/ui/main/MainActivity.kt
+++ app/src/main/java/com/aldreduser/gymroutine/ui/main/MainActivity.kt
@@ -2,7 +2,15 @@ package com.aldreduser.gymroutine.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.aldreduser.gymroutine.R
+
+// - todo: major bug:
+//      when updating reps and weight in a workout in workouts list fragment. all reps and weight
+//      in that workout are updated to the same in that set. This works fine in edit screen.
+//      Maybe it's bc i have the custom list adapter in the edit fragment when i edit the first
+//      set weight and add another set and edit that weight, the first set weight is set
+//      to the weight of the new set.
 
 // todo: Display the workouts in a custom position
 //   have a position attribute for the workout entities
@@ -18,6 +26,7 @@ import com.aldreduser.gymroutine.R
 // todo: clean up unused imports
 
 // Future
+// add a workout note in the edit screen (user can write notes)
 // Ask the user before deleting an item
 // Cap the groups at about 30
 // When editing a new group, have the group name show up on the spinner
@@ -30,5 +39,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
